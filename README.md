aa366a2907e21a0b8b3e13dde075e86b307f4aaa
Alexander Kazakov (Mint, native) <alexander@kazakov.mobi>
2013-09-24 01:38:25 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby on Rails Turorial: sample application
+
+Sample App # 2 for [Ruby on Rails Tutorial](http://railstutorial.org/) by M. Hartl.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondSample::Application.config.secret_token = '99e77e265544e1ae30f4ee19d08c8daf1adad426ef769865db7b59dfe577e1638a27ee7fc4f662f680a7c12b31f4270e50a2a67153275b25e79c2f48ef348730'
