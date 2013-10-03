bc94f83b79f4602d40599dfb5f0878b2411d4b3c
Alexander Kazakov (Mint, native) <alexander@kazakov.mobi>
2013-10-03 05:23:53 -0700

    Finish layout and routes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,11 +1,2 @@
 module StaticPagesHelper
-
-  def full_title(page_title = '')
-
-    title = "Ruby on Rails Tutorial Sample App"
-    title += " | #{page_title}" unless page_title.empty?
-    title
-
-  end
-
 end
