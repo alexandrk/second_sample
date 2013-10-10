577ef44d18ca25e880064f736bfed866f9d3c19e
Alexander Kazakov (Mint, native) <alexander@kazakov.mobi>
2013-10-10 04:15:20 -0700

    Fix improper (singular) naming of User[s] controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ SecondSample::Application.routes.draw do
 
   root              to: 'static_pages#home'
   
-  match "/signup",   to: 'user#new'
+  match "/signup",  to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
